0f526806b7d576aa163dcb6b5c33b127237dbc6b
NDrLEUxcwQsgKUWlrFELocR1z7QP <54683950+maxnut@users.noreply.github.com>
2024-05-04 21:21:45 +0200

    [DL-29] student filter

--- src/main/java/it/justsport/api/endpoint/TournamentSubscribeEndpoint.java
+++ src/main/java/it/justsport/api/endpoint/TournamentSubscribeEndpoint.java
@@ -1,15 +1,24 @@
 package it.justsport.api.endpoint;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+import com.google.gson.Gson;
+
+import it.justsport.api.Responses;
+import it.justsport.api.Responses.Response;
+import it.justsport.api.dao.TeamDAO;
+import it.justsport.api.dao.TournamentDAO;
+import it.justsport.api.table.Team;
+import it.justsport.api.table.Tournament;
+import it.justsport.api.table.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-import it.justsport.api.Responses;
-import it.justsport.api.Responses.Response;
-
+@WebServlet("/tournament/subscribe")
 public class TournamentSubscribeEndpoint extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -38,6 +47,37 @@ public class TournamentSubscribeEndpoint extends HttpServlet {
 			return;
 		}
 		
+		try {
+			Tournament t = TournamentDAO.getTournamentByID(Long.valueOf(tournamentID));
+			Team team = TeamDAO.getTeamByID(Long.valueOf(teamID));
+			
+			if(t == null || team == null)
+			{
+				Responses.respond(response, Response.SERVER_ERROR);
+				return;
+			}
+			
+			if(t.type == "extrascolastico")
+			{
+				for(User u : team.members)
+				{
+					if(u.type != "stu")
+					{
+						Responses.respond(response, Response.BAD_REQUEST);
+						return;
+					}
+				}
+			}
+			
+			Gson gson = new Gson();
+			
+			TournamentDAO.addTeamToTournament(t, team);
+			
+			Responses.respondWithObject(response, Response.OK, gson.toJsonTree(t));
+		} catch (NumberFormatException | ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
